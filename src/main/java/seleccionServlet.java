73b77d30ccb4eccbdea9d0d93c8c571563bb0e83
SergioArispeJulio <sergio_arispe@hotmail.com>
2013-12-03 07:55:05 -0400

    respuesta 3

---
+++ src/main/java/seleccionServlet.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class seleccionServlet extends HttpServlet {
+
+
+	@Override
+	protected void service(HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException {
+	PrintWriter out = response.getWriter();
+	out.println("<html>");
+	out.println("<head>");
+	out.println("</head>");
+	out.println("<body>");	
+		out.println("Anio: "+ request.getParameter("anio")+"<br>");
+		out.println("Mes: "+ request.getParameter("mes")+"<br>");
+		out.println("Dia: "+ request.getParameter("dia")+"<br>");
+	out.println("</body>");
+	out.println("</html>");
+
+	}
+	
+}
