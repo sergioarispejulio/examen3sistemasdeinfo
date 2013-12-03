1b837c0e0a3dc549ebfefa8b15971195738d8496
SergioArispeJulio <sergio_arispe@hotmail.com>
2013-12-03 08:10:41 -0400

    Respuesta 4.1 -considerar este

--- src/test/java/LogicaTEST.java
+++ src/test/java/LogicaTEST.java
@@ -12,5 +12,22 @@ public class LogicaTEST {
 	public void pruebadecondicion1deaniobisiestoquesupera1() {
 		assertEquals(true, verificador.verificacionBisiesto(1992));
 	}
+	
+	@Test
+	public void pruebadecondicion1deaniobisiestoquesupera2() {
+		assertEquals(true, verificador.verificacionBisiesto(40));
+	}
+	
+	@Test
+	public void pruebadecondicion1deaniobisiestoquenosupera1() {
+		assertEquals(false, verificador.verificacionBisiesto(1994));
+	}
+	
+	@Test
+	public void pruebadecondicion1deaniobisiestoquenosupera2() {
+		assertEquals(false, verificador.verificacionBisiesto(100));
+	}
+	
+	
 
 }
